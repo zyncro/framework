df5b85167b5bc8bb51b083921aa8087a34818eed
Luis Mas Serral <luis.mas@zyncro.com>
2015-03-07 19:23:30 +0100

    First commit

---
+++ Core.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zyncro\Framework;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class Core implements HttpKernelInterface
+{
+    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+    {
+        switch ($request->getPathInfo()) {
+            case '/':
+                $response = new Response('This is the website home');
+                break;
+
+            case '/about':
+                $response = new Response('This is the about page');
+                break;
+
+            default:
+                $response = new Response('Not found !', Response::HTTP_NOT_FOUND);
+        }
+
+        return $response;
+    }
+}
