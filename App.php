9d2cf98ac366fe5de9808986d9c624d143cf2245
Luis Mas Serral <luis.mas@zyncro.com>
2015-03-07 22:06:47 +0100

    Renamed

--- App.php
+++ App.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-class Core implements HttpKernelInterface
+class App implements HttpKernelInterface
 {
     public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
     {
